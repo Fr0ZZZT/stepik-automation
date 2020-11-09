ad5622d809f3de3a59dd7f0c0d51e6815d583da3
Fr0ZZZT <newkosss@gmail.com>
2020-11-09 16:34:43 +0200

    pre-conditions to final exam

---
+++ conftest.py
@@ -0,0 +1,21 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=en,
+                     help="Choose a language for testing")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    language = request.config.getoption("language")
+    if language is not None:
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
+        browser.implicitly_wait(10)
+    else:
+        raise pytest.UsageError("--language should be selected")
+    yield browser
+    browser.quit()
